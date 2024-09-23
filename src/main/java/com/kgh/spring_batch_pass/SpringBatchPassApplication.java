982d7de031bd2b5c7ecfe6e9c092a5f8375c62cd
kgh <gwanhyeon0525@gmail.com>
2024-09-23 23:16:10 +0900

    feat: spring batch step and job config

--- src/main/java/com/kgh/spring_batch_pass/SpringBatchPassApplication.java
+++ src/main/java/com/kgh/spring_batch_pass/SpringBatchPassApplication.java
@@ -9,7 +9,6 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,8 +18,31 @@ import org.springframework.context.annotation.Bean;
 
 public class SpringBatchPassApplication {
 
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
+    public SpringBatchPassApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
 
+    @Bean
+    public Step passStep(){
+        return this.stepBuilderFactory.get("passStep")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Execute PassStep");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    @Bean public Job passJob(){
+        return this.jobBuilderFactory.get("passJob")
+                .start(passStep())
+                .build();
+    }
 
     public static void main(String[] args) {
 		SpringApplication.run(SpringBatchPassApplication.class, args);
