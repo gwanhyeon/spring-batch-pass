70fa5cc026ba9e80c4888b715f4e28e3ffa7f830
kgh <gwanhyeon0525@gmail.com>
2024-09-23 22:37:14 +0900

    chore: batch project init

---
+++ src/main/java/com/kgh/spring_batch_pass/SpringBatchPassApplication.java
@@ -0,0 +1,13 @@
+package com.kgh.spring_batch_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPassApplication.class, args);
+	}
+
+}
